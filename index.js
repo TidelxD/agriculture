d9200a5a521259a938ca310a4e5ffb080528dd3b
TidelxD <luffygear37@gmail.com>
2022-04-19 22:18:13 +0000

    Add Authentication ( JSON WEB TOKEN )

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express= require('express');
 const mysql = require('mysql');
 
 const login = require('./logger/logging')
+const { checkToken } = require('./auth/token_validation')
 
 const app = express();
 
+// method for JWT 
+const { sign } = require('jsonwebtoken');
 
 app.use(express.json());
 app.use(login );
+
 
 // create connections ...
 const db = mysql.createConnection({
@@ -58,7 +62,7 @@ const db = mysql.createConnection({
  // ********************************** Creating ending ************************** // 
  
  // Add Row in sensors 
- app.get("/addSensorsrow/:temperature/:humidity/:soilmoaster/:windSpeed/:x/:y",(req,res)=>{
+ app.get("/addSensorsrow/:temperature/:humidity/:soilmoaster/:windSpeed/:x/:y",checkToken,(req,res)=>{
   let sql = "INSERT INTO sensors (temperature, humidity, timestamp,soilmoaster,windSpeed,x,y ) VALUES (?,?,?,?,?,?,?)";
   var values = [req.params.temperature,req.params.humidity,"04-09-2022 13:51",req.params.soilmoaster,req.params.windSpeed,req.params.x,req.params.y];
 
@@ -72,7 +76,7 @@ const db = mysql.createConnection({
 
 
  // Get ALL Data from sensors
-/* app.get("/getAllData",(req,res)=>{
+/* app.get("/getAllData",checkToken,(req,res)=>{
   let sql ="SELECT * FROM sensors";
    let query = db.query(sql,(err,result)=>{
      if(err) throw err;
@@ -94,7 +98,7 @@ const db = mysql.createConnection({
  });*/
 
 // Calculate temperature moyen
- app.get("/temp_moy",(req,res)=>{
+ app.get("/temp_moy",checkToken,(req,res)=>{
     let sql ="SELECT sum(temperature)/count(*) FROM sensors ";
 
        db.query(sql,(err,result)=>{
@@ -106,7 +110,7 @@ const db = mysql.createConnection({
  });
 
  // Getting last Data
- app.get("/getLastDataSensors",(req,res)=>{
+ app.get("/getLastDataSensors",checkToken,(req,res)=>{
   let sql ="SELECT * FROM sensors ORDER BY id DESC LIMIT 1";
 
      db.query(sql,(err,result)=>{
@@ -120,7 +124,7 @@ const db = mysql.createConnection({
 /******************************************* ACTUTORS ******************************************************** */
 
  // Add Row in actutors
- app.get("/addActutorsrow/:type/:x/:y/:state",(req,res)=>{
+ app.get("/addActutorsrow/:type/:x/:y/:state",checkToken,(req,res)=>{
   let sql = "INSERT INTO actutors (type, x, y,state,timestamp) VALUES (?,?,?,?,?)";
   var values = [req.params.type,req.params.x,req.params.y,req.params.state,Date.now()];
 
@@ -128,12 +132,12 @@ const db = mysql.createConnection({
     if(err) throw err;
     console.log("inserted");
     res.send(values);
-  })
+  });
  });
 
 
  // Getting Data for spesific actutor
- app.get("/getSpecActutors/:id",(req,res)=>{
+ app.get("/getSpecActutors/:id",checkToken,(req,res)=>{
   let sql ="SELECT * FROM actutors WHERE id ="+req.params.id;
 
      db.query(sql,(err,result)=>{
@@ -144,18 +148,106 @@ const db = mysql.createConnection({
 
 }); 
 
-// Update Actutor State 
-app.get("/UpdateState/:id/:state",(req,res)=>{
-   let sql = "UPDATE actutors SET state="+req.params.state+" WHERE id="+req.params.id;
-   db.query(sql,(err,result)=>{
+// Update Actutor State with insert query
+app.get("/UpdateStateIns/:id/:type/:x/:y/:state",checkToken,(req,res)=>{
+  let sql = "INSERT INTO actutors (id,type, x, y,state) VALUES (?,?,?,?,?)";
+  var values = [req.params.id,req.params.type,req.params.x,req.params.y,req.params.state];
+
+  db.query(sql,values,(err,result)=>{
     if(err) throw err;
-     console.log("update id is: "+req.params.id);
-     res.send(result);
- });  
+    console.log("inserted");
+    res.send(values);
+  });
+
+});
+
+
+
+// Update Actutor State with insert query 
+app.get("/UpdateState/:id/:state",checkToken,(req,res)=>{
+  let sql = "UPDATE users SET state="+req.params.state+" WHERE id="+req.params.id;
+  db.query(sql,(err,result)=>{
+   if(err) throw err;
+    console.log("update id is: "+req.params.id);
+    res.send(result);
+});  
 
 
 });
 
+
+
+// *********************************************** USERS **************************************************//
+
+ // method for crypt and compare the passwords
+
+const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+ // Create User 
+ app.get("/creatuser/:username/:password",(req,res)=>{
+  
+  let sql = "INSERT INTO users (username, password) VALUES (?,?)";
+  const salt =  genSaltSync(10);
+  req.params.password = hashSync(req.params.password,salt);
+  var values = [req.params.username,req.params.password];
+  
+  db.query("SELECT * FROM users WHERE username ="+req.params.username,(err,result)=>{
+    if(err) throw err;
+    if(result){
+        return res.json({
+          success: 0,
+          data: "this username already used by another account !"
+        });
+
+    }
+    db.query(sql,values,(err,result)=>{
+      if(err) throw err;
+      console.log("inserted");
+      res.send(result);
+    });
+   
+  });
+  
+ });
+
+ // Get User by username
+ app.get("/login/:username/:password",(req,res)=>{
+
+  let sql ="SELECT * FROM users WHERE username ="+req.params.username;
+  
+  db.query(sql,(err,result)=>{
+
+    if(err) throw err; 
+    if (!result[0]) {
+      return res.json({
+        success: 0,
+        data: "Invalid email or password"
+      });
+    }
+    console.log("req.params.password: "+req.params.password);
+    console.log("\n result.password: "+result[0].password);
+   const results = compareSync(req.params.password, result[0].password);
+    if(results){
+       result.password = undefined;
+      const jsontoken = sign({ results: result }, "qwe1234", {
+        expiresIn: "1h"
+      }); 
+      return res.json({
+        success: 1,
+        message: "login successfully",
+        token : jsontoken
+      });
+
+    }else {
+      return res.json({
+        success: 0,
+        data: "Invalid email or password"
+      });
+    } 
+    
+   
+  });
+  
+ });
 
 
 
